ac9e353dd2917a326222c4225a135de900643e76
Max Jacobson <max@hardscrabble.net>
2021-03-09 22:57:35 -0500

    Set up react

--- app/javascript/entrypoints/application.ts
+++ app/javascript/entrypoints/application.ts
@@ -1,22 +1,8 @@
-// To see this message, add the following to the `<head>` section in your
-// views/layouts/application.html.erb
-//
-//    <%= vite_client_tag %>
-//    <%= vite_javascript_tag 'application' %>
-console.log('Vite ⚡️ Rails')
+import React from "react"
+import { render } from "react-dom"
+import App from "./App"
 
-// Example: Load Rails libraries in Vite.
-//
-// import '@rails/ujs'
-//
-// import Turbolinks from 'turbolinks'
-// import ActiveStorage from '@rails/activestorage'
-//
-// // Import all channels.
-// import.meta.globEager('./**/*_channel.js')
-//
-// Turbolinks.start()
-// ActiveStorage.start()
-
-// Example: Import a stylesheet in app/frontend/index.css
-// import '~/index.css'
+render(
+  React.createElement(App, {}, null),
+  document.getElementById("app"),
+)
