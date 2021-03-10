86442ca4cd994c109e3fdda9151523b7d777a717
Max Jacobson <max@hardscrabble.net>
2021-03-09 23:06:36 -0500

    add a routing library

--- app/javascript/entrypoints/App.tsx
+++ app/javascript/entrypoints/App.tsx
@@ -1,7 +1,21 @@
 import React from "react"
+import { Link, Router } from "@reach/router"
+import Home from "./pages/Home"
+import Dashboard from "./pages/Dashboard"
 
 const App = () => (
-  <h1>Hello world</h1>
+  <>
+    <nav>
+      <Link to="/">Home</Link>{" "}
+      <Link to="dashboard">Dashboard</Link>
+    </nav>
+    <h1>Hello world</h1>
+
+    <Router>
+      <Home path="/" />
+      <Dashboard path="/dashboard" />
+    </Router>
+  </>
 );
 
 export default App
---
+++ app/javascript/entrypoints/pages/Home.tsx
@@ -0,0 +1,3 @@
+import React from "react"
+const Home = () => <p>Home</p>
+export default Home
