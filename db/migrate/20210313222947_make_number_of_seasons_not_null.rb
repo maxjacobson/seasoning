ff97885ded920f7db943a95d524f2b5ae6427a3d
Max Jacobson <max@hardscrabble.net>
2021-03-13 17:31:40 -0500

    Make shows.number_of_seasons not nullable

---
+++ db/migrate/20210313222947_make_number_of_seasons_not_null.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# We can do this now because it's such early days...
+# later on we would need to do this more carefully
+class MakeNumberOfSeasonsNotNull < ActiveRecord::Migration[6.1]
+  def change
+    Show.where(number_of_seasons: nil).destroy_all
+    change_column_null :shows, :number_of_seasons, false
+  end
+end
