00fa2cb3ae9c00b16ab035a7bdfaeffb478135a6
Max Jacobson <max@hardscrabble.net>
2021-03-14 14:09:26 -0400

    Add a seeds file
    Just to help populate the db with some TV shows in the event that I drop
    the database and want to repopulate it

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,20 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+[
+  "https://en.wikipedia.org/wiki/Zoey%27s_Extraordinary_Playlist",
+  "https://en.wikipedia.org/wiki/The_Sopranos",
+  "https://en.wikipedia.org/wiki/Halt_and_Catch_Fire_(TV_series)",
+  "https://en.wikipedia.org/wiki/Mad_Men",
+  "https://en.wikipedia.org/wiki/It%27s_Always_Sunny_in_Philadelphia",
+  "https://en.wikipedia.org/wiki/What_We_Do_in_the_Shadows_(TV_series)",
+  "https://en.wikipedia.org/wiki/The_Magicians_(American_TV_series)"
+].each do |wikipedia_url|
+  wikipedia_page = Wikipedia::TVShowPage.new(wikipedia_url)
+  next if Show.find_by(title: wikipedia_page.title).present?
+
+  Show.create!(
+    title: wikipedia_page.title,
+    wikipedia_page_id: wikipedia_page.page_id,
+    number_of_seasons: wikipedia_page.number_of_seasons
+  )
+end
