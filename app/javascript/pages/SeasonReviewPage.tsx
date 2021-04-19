e6dfede07b745e8cfcd9224c918fc14eb280f5ba
Max Jacobson <max@hardscrabble.net>
2021-04-18 23:36:30 -0400

    Fix zero-is-falsey bug

--- app/javascript/pages/SeasonReviewPage.tsx
+++ app/javascript/pages/SeasonReviewPage.tsx
@@ -129,7 +129,7 @@ const SeasonReviewPage: FunctionComponent<Props> = ({
         <Card.Section title="Date">
           {DateTime.fromISO(review.created_at).toLocaleString()}
         </Card.Section>
-        {review.rating && <Card.Section title="Rating">{review.rating}</Card.Section>}
+        {review.rating !== undefined && <Card.Section title="Rating">{review.rating}</Card.Section>}
         <Card.Section>
           <Spoilers spoilers={review.spoilers}>
             <Markdown markdown={review.body} />
