9ac4e0f8e5fdc914c4b2e137581efc777e2a3f16
Max Jacobson <max@hardscrabble.net>
2021-10-24 15:20:22 -0400

    Add new "next up" show status (#253)
    Let's you make a little more of a plan for yourself.
    
    May also automatically move things into next up when new seasons become
    available... :thinking:

--- app/models/my_show.rb
+++ app/models/my_show.rb
@@ -5,6 +5,7 @@ class MyShow < ApplicationRecord
   belongs_to :show
   enum status: {
     might_watch: "might_watch",
+    next_up: "next_up",
     currently_watching: "currently_watching",
     stopped_watching: "stopped_watching",
     waiting_for_more: "waiting_for_more",
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_02_011508) do
+ActiveRecord::Schema.define(version: 2021_10_24_190928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_05_02_011508) do
     "stopped_watching",
     "waiting_for_more",
     "finished",
+    "next_up",
   ], force: :cascade
 
   create_enum :visibility, [
