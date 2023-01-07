48f1ec72adbdba1b3cf2ce2f4d73eb9f81bf8460
Max Jacobson <max@hardscrabble.net>
2023-01-07 14:43:26 -0500

    Stop importing episodes 🫠 (#836)
    I forgot I only can have 10k rows on my "mini" heroku plan... will need
    to reevaluate

--- app/services/refresh_episodes.rb
+++ app/services/refresh_episodes.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
-RefreshEpisodes = lambda do |show, season|
-  data = TMDB::Client.new.season_details(show.tmdb_tv_id, season.season_number)
+RefreshEpisodes = lambda do |_show, _season|
+  Rails.logger.info "Skipping episodes..."
 
-  data.episodes.each do |tmdb_episode|
-    episode = season.episodes.find_or_initialize_by(tmdb_id: tmdb_episode.id)
-    episode.name = tmdb_episode.name
-    episode.still_path = tmdb_episode.still_path
-    episode.episode_number = tmdb_episode.episode_number
-    episode.save!
-  end
+  # data = TMDB::Client.new.season_details(show.tmdb_tv_id, season.season_number)
+  # data.episodes.each do |tmdb_episode|
+  #   episode = season.episodes.find_or_initialize_by(tmdb_id: tmdb_episode.id)
+  #   episode.name = tmdb_episode.name
+  #   episode.still_path = tmdb_episode.still_path
+  #   episode.episode_number = tmdb_episode.episode_number
+  #   episode.save!
+  # end
 end
