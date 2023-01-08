6dc5dae0fd165d01dc275d94179b7272f13c0175
Max Jacobson <max@hardscrabble.net>
2023-01-07 20:17:01 -0500

    Mark still_sizes as not nullable

---
+++ db/migrate/20230108011418_mark_still_sizes_as_not_nullable.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Now that we've set this value, we can mark it as not nullable going forward
+class MarkStillSizesAsNotNullable < ActiveRecord::Migration[7.0]
+  def change
+    change_column_null :tmdb_api_configurations, :still_sizes, false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_08_010610) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_08_011418) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -153,7 +153,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_08_010610) do
     t.string "poster_sizes", null: false, array: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "still_sizes", comment: "Available sizes for episode stills", array: true
+    t.string "still_sizes", null: false, comment: "Available sizes for episode stills", array: true
     t.index ["poster_sizes"], name: "index_tmdb_api_configurations_on_poster_sizes", using: :gin
   end
 
