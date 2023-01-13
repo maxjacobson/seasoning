de822f290a21ef250793f7d3390e0b49d76d0e3a
Max Jacobson <max@hardscrabble.net>
2023-01-13 11:01:37 -0500

    Report heroku release version to bugsnag (#871)

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -3,4 +3,5 @@
 Bugsnag.configure do |config|
   config.api_key = ENV.fetch("BUGSNAG_API_KEY")
   config.enabled_release_stages = ["production"]
+  config.app_version = ENV.fetch("HEROKU_RELEASE_VERSION", nil)
 end
