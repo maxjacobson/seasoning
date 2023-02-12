a82eff430821c51fd7587e1f3e69654ca0ad12c9
Max Jacobson <max@hardscrabble.net>
2023-02-11 22:28:00 -0500

    Fix episode refresh bugs (#926)
    This is kind of a crazy inefficient solution, but idc... it's the
    hammer.

--- app/services/refresh_episodes.rb
+++ app/services/refresh_episodes.rb
@@ -3,8 +3,9 @@
 RefreshEpisodes = lambda do |show, season|
   data = TMDB::Client.new.season_details(show.tmdb_tv_id, season.season_number)
 
+  season.episodes.destroy_all
   data.episodes.each do |tmdb_episode|
-    episode = season.episodes.find_or_initialize_by(episode_number: tmdb_episode.episode_number)
+    episode = season.episodes.new(episode_number: tmdb_episode.episode_number)
     episode.name = tmdb_episode.name
     episode.still_path = tmdb_episode.still_path
     episode.tmdb_id = tmdb_episode.id
