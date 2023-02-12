bb9a3c6d099ad5656c1a0aa99e5c527f4f9e724a
Max Jacobson <max@hardscrabble.net>
2023-02-11 21:34:38 -0500

    Streamline credits page (#924)

--- app/javascript/pages/CreditsPage.tsx
+++ app/javascript/pages/CreditsPage.tsx
@@ -6,33 +6,23 @@ export const CreditsPage: FunctionComponent = () => {
 
   return (
     <div>
-      <h1>Credits</h1>
+      <h1 className="text-2xl">Credits</h1>
 
-      <div>
-        <h2>Inspiration</h2>
-
-        <p>
+      <ul className="list-inside list-disc">
+        <li>
           Obviously this is very much inspired by{" "}
           <a href="https://letterboxd.com/" target="_blank" rel="noreferrer">
             Letterboxd
           </a>
           .
-        </p>
-      </div>
-
-      <div>
-        <h2>Credits</h2>
-        <h3>Data</h3>
-        <p>
-          All data from{" "}
+        </li>
+        <li>
+          All of the data is sourced from{" "}
           <a href="https://www.themoviedb.org/" target="_blank" rel="noreferrer">
             The Movie Database
           </a>
-          .
-        </p>
-
-        <h3>Development</h3>
-        <p>
+        </li>
+        <li>
           This site is developed by{" "}
           <a href="https://www.hardscrabble.net" target="_blank" rel="noreferrer">
             Max Jacobson
@@ -43,14 +33,12 @@ export const CreditsPage: FunctionComponent = () => {
           </a>
           . Feel free to pitch in and/or roast me. There are no tests. Just going to get ahead of
           that one. Otherwise it&rsquo;s perfect.
-        </p>
-
-        <h3>Icon</h3>
-        <p>
+        </li>
+        <li>
           The television icon you see up top is{" "}
           <a href="https://www.toicon.com/icons/avocado_watch">by Shannon E Thomas</a>.
-        </p>
-      </div>
+        </li>
+      </ul>
     </div>
   )
 }
