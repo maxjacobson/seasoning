ef36d62a07b2d27567c69acb19182346d22d117a
Max Jacobson <max@hardscrabble.net>
2023-02-14 15:48:44 -0500

    Make the demo responsive (#942)

---
+++ app/javascript/components/YouTubeVideo.tsx
@@ -0,0 +1,18 @@
+type Props = {
+  id: string
+}
+
+export const YouTubeVideo = ({ id }: Props) => {
+  return (
+    <div className="embed-container">
+      <iframe
+        width="560"
+        height="315"
+        src={`https://www.youtube-nocookie.com/embed/${id}`}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
+    </div>
+  )
+}
