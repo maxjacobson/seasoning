0015984401b06ea281a393cbfc99695869b62e12
Max Jacobson <max@hardscrabble.net>
2023-02-23 16:04:45 -0500

    Improve styling of the global loading ribbon
    This is animated now

--- app/javascript/components/LoadingRibbon.tsx
+++ app/javascript/components/LoadingRibbon.tsx
@@ -1,13 +1,3 @@
-import { FunctionComponent } from "react"
-
-interface Props {
-  loading: boolean
-}
-
-export const LoadingRibbon: FunctionComponent<Props> = ({ loading }: Props) => {
-  if (loading) {
-    return <div className="absolute top-0 left-0 h-2.5 w-full bg-slate-500" />
-  } else {
-    return <></>
-  }
+export const LoadingRibbon = () => {
+  return <div className="loading-ribbon absolute top-0 left-0 h-2.5 bg-slate-500" />
 }
