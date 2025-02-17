cafc6246e396a1eeb7154dfa4bb5dac1786e843e
Max Jacobson <max@hardscrabble.net>
2025-02-17 16:05:06 -0500

    Address rails warning

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,8 @@ module Seasoning
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.active_support.to_time_preserves_timezone = :zone
+
     # Don't generate system test files.
     config.generators.system_tests = nil
 
