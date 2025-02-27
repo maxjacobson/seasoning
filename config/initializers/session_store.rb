e94bbd9262ce5aaad8e1e0b68f97f9c9b1dd1425
Max Jacobson <max@hardscrabble.net>
2025-02-27 18:29:43 -0500

    expire session less frequently

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_seasoning_session"
+Rails.application.config.session_store(
+  :cookie_store,
+  key: "_seasoning_session",
+  expires_after: 3.months
+)
