40194eafe8f9382c68e1eb8a7455a65df8a54335
Max Jacobson <max@hardscrabble.net>
2025-04-03 19:41:08 -0400

    Store rails sessions in the db
    I keep getting logged out in mobile, maybe this will help? I don't
    actually have a hypothesis but it seems worth trying.

---
+++ db/migrate/20250403233742_add_sessions_table.rb
@@ -0,0 +1,13 @@
+# Store sessions in the db
+class AddSessionsTable < ActiveRecord::Migration[8.0]
+  def change
+    create_table :sessions do |t|
+      t.string :session_id, null: false
+      t.text :data
+      t.timestamps
+    end
+
+    add_index :sessions, :session_id, unique: true
+    add_index :sessions, :updated_at
+  end
+end
