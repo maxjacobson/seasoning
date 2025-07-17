d285b28a9bf72d075759c0b9f2a0b29035360aa2
Max Jacobson <max@hardscrabble.net>
2025-07-16 23:44:07 -0400

    Track code coverage with minimums

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,13 @@
 ENV["RAILS_ENV"] ||= "test"
+
+if ENV["COVERAGE"]
+  require "simplecov"
+  SimpleCov.start "rails" do
+    minimum_coverage 77
+    minimum_coverage_by_file 0
+  end
+end
+
 require_relative "../config/environment"
 require "rails/test_help"
 require "webmock/minitest"
@@ -8,8 +17,8 @@ WebMock.disable_net_connect!(allow_localhost: true)
 module ActiveSupport
   # Global test setup/configuration
   class TestCase
-    # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors)
+    # Run tests in parallel with specified workers (disabled when collecting coverage)
+    parallelize(workers: :number_of_processors) unless ENV["COVERAGE"]
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
