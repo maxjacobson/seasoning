908b80f0bcdb32fcbcac60f0a602fa40508a07da
Max Jacobson <max@hardscrabble.net>
2025-10-09 20:11:00 -0400

    Update list of reviews

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -7,5 +7,8 @@ class SeasonsController < ApplicationController
     @season = @show.seasons.find_by!(slug: params[:season_slug])
     @my_season = MySeason.find_or_initialize_by(human: current_human, season: @season)
     @episodes = @season.episodes.order(episode_number: :asc)
+    @season_reviews = @season.season_reviews.viewable_by(current_human)
+                             .includes(:author)
+                             .order(created_at: :desc)
   end
 end
