8cdedcbea85b3864ea3d924f04e585778e266b66
Max Jacobson <max@hardscrabble.net>
2026-02-05 17:41:15 -0500

    Track orphaned shows and seasons

--- app/services/refresh_show.rb
+++ app/services/refresh_show.rb
@@ -1,13 +1,21 @@
 RefreshShow = lambda { |show|
-  Show.transaction do
+  begin
     details = TMDB::Client.new.tv_details(show.tmdb_tv_id)
+  rescue TMDB::Client::NotFound
+    show.update!(orphaned: true)
+    next
+  end
 
+  Show.transaction do
     show.update!(
       tmdb_poster_path: details.poster_path,
       tmdb_next_refresh_at: Show.refresh_interval(details),
       first_air_date: details.first_air_date,
-      tmdb_last_refreshed_at: Time.zone.now
+      tmdb_last_refreshed_at: Time.zone.now,
+      orphaned: false
     )
+
+    seen_season_ids = []
 
     details.seasons.each do |tmdb_season|
       next if tmdb_season.season_number.zero?
@@ -21,7 +29,8 @@ RefreshShow = lambda { |show|
           episode_count: tmdb_season.episode_count,
           tmdb_id: tmdb_season.id,
           tmdb_poster_path: tmdb_season.poster_path,
-          air_date: tmdb_season.air_date
+          air_date: tmdb_season.air_date,
+          orphaned: false
         )
       else
         season = show.seasons.create!(
@@ -33,7 +42,12 @@ RefreshShow = lambda { |show|
           air_date: tmdb_season.air_date
         )
       end
+      seen_season_ids << season.id
       RefreshEpisodes.call(show, season)
+    end
+
+    show.seasons.where.not(id: seen_season_ids).find_each do |season|
+      season.update!(orphaned: true)
     end
   end
 }
